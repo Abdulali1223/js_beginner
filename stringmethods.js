cca8283a6643e09c9f5e940b19f42532e5e4b39d
Abdulali1223 <147648620+Abdulali1223@users.noreply.github.com>
2023-10-16 12:38:38 +0100

    Add files via upload

---
+++ stringmethods.js
@@ -0,0 +1,6 @@
+let email = 'abdulali1223@icloud.com';
+//let result = email.lastIndexOf('a');
+//let result = email.slice(0,8)
+//let result = email.substr(0,10)
+let result = email.replace("a","i")
+console.log(result)
